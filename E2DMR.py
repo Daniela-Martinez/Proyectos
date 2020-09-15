f13d32835a10d4b545fdbe42f8021b009b0fd60e
Daniela-Martinez <d.rdz.240615@gmail.com>
2020-09-14 22:18:05 -0500

    Tarea

---
+++ E2DMR.py
@@ -0,0 +1,38 @@
+﻿copyright
+import requests
+import os
+import sys
+from bs4 import BeautifulSoup as bs
+import webbrowser
+
+# Codigo modificado por Daniela Martinez Rodriguez.
+# Este script entra a las noticias de la UANL y te pide desde que pagina quieres iniciar tu busqueda y hasta cual finalizar, despues te pide las siglas de alguna facultad perteneciente a la UANL y arroja las URL que obvtiene en su busqueda en el en el rango de paginas que se ingreso.
+print("Este script navega en las páginas de noticas de la UANL")
+inicioRango = int(input("Pagina inicial para buscar: "))
+finRango = int(input("Pagina final para buscar: "))
+dependencia = input("Ingrese las siglas de la Facultad a buscar: ")
+if inicioRango > finRango:
+    inicioRango,finRango = finRango,inicioRango
+for i in range (inicioRango,finRango,1):
+    url = "https://www.uanl.mx/noticias/page/"+str(i)
+    pagina = requests.get (url)
+    if pagina.status_code != 200:
+        raise TypeError("Pagina no encontrada")
+    else:
+        soup = bs(pagina.content,"html.parser")
+        info = soup.select("h3 a")
+        for etiqueta in info:
+            url2 = etiqueta.get("href")
+            pagina2 = requests.get(url2)
+            if pagina2.status_code == 200:
+                soup2 = bs(pagina2.content,"html.parser")
+                parrafos = soup2.select("p")    
+                for elemento in parrafos:
+                    if dependencia in elemento.getText():
+                        print ("Abriendo",url2)
+                        webbrowser.open(url2)
+                        break
+
+
+    
+
